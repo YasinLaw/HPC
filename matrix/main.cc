5222b3ae5b785d756705e4c58cd4c0d69b95d254
Luo Yangxin <yasinlaw@outlook.com>
2023-12-30 13:26:05 +0800

    final commit

--- matrix/main.cc
+++ matrix/main.cc
@@ -1,82 +1,79 @@
-#include "mpi.h"
+#include <chrono>
 #include <cstdio>
 #include <random>
-#include <chrono>
+
+#include "mpi.h"
 
 constexpr int len = 1024;
 
-int main(int argc, char* argv[])
-{
-	int rank, size;
-	MPI_Status status;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+int main(int argc, char *argv[]) {
+  int rank, size;
+  MPI_Status status;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	const int matrix_size = len * len;
-	const int chunk_size = matrix_size / size;
+  const int matrix_size = len * len;
+  const int chunk_size = matrix_size / size;
 
-	double* a;
-	double* b = new double[matrix_size];
-	double* chunk = new double[chunk_size];
-	double* results;
-	double result;
-	double* chunk_results = new double[chunk_size];
-	auto start = std::chrono::steady_clock::now();
+  double *a;
+  double *b = new double[matrix_size];
+  double *chunk = new double[chunk_size];
+  double *results;
+  double result;
+  double *chunk_results = new double[chunk_size];
+  auto start = std::chrono::steady_clock::now();
 
-	if (rank == 0) 
-	{
-		// initialize the matrix with random floating point number
-		a = new double[matrix_size];
-		results = new double[matrix_size];
-		std::random_device rd;
-		std::mt19937 mt(rd());
-		std::uniform_real_distribution<double> dist(-1.0, 1.0);
-		for (int i = 0; i < len; i++) 
-		{
-			for (int j = 0; j < len; j++)
-			{
-				a[i * len + j] = dist(mt);
-				b[j * len + i] = dist(mt);
-			}
-		}
-		start = std::chrono::steady_clock::now();
-	}
+  if (rank == 0) {
+    // initialize the matrix with random floating point number
+    a = new double[matrix_size];
+    results = new double[matrix_size];
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(-1.0, 1.0);
+    for (int i = 0; i < len; i++) {
+      for (int j = 0; j < len; j++) {
+        a[i * len + j] = dist(mt);
+        b[j * len + i] = dist(mt);
+      }
+    }
+    start = std::chrono::steady_clock::now();
+  }
 
-	MPI_Scatter(a, chunk_size, MPI_DOUBLE, chunk, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	MPI_Bcast(b, matrix_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Scatter(a, chunk_size, MPI_DOUBLE, chunk, chunk_size, MPI_DOUBLE, 0,
+              MPI_COMM_WORLD);
+  MPI_Bcast(b, matrix_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-	int width = len;
-	int height = len / size;
+  int width = len;
+  int height = len / size;
 
-	int index = 0;
-	for (int r = 0; r < height; r++)
-	{
-		for (int c = 0; c < width; c++)
-		{
-			result = 0;
-			for (int k = 0; k < len; k++)
-			{
-				result += chunk[r * len + k] * b[k * len + c];	
-			}
-			chunk_results[index++] = result;
-		}
-	}
+  int index = 0;
+  for (int r = 0; r < height; r++) {
+    for (int c = 0; c < width; c++) {
+      result = 0;
+      for (int k = 0; k < len; k++) {
+        result += chunk[r * len + k] * b[k * len + c];
+      }
+      chunk_results[index++] = result;
+    }
+  }
 
-	MPI_Gather(chunk_results, chunk_size, MPI_DOUBLE, results + rank * chunk_size, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Gather(chunk_results, chunk_size, MPI_DOUBLE, results + rank * chunk_size,
+             chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-	if (rank == 0)
-	{
-		auto end = std::chrono::steady_clock::now();
-		auto elapsed = end - start;
-		printf("task costs %lld μs\n", std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count());
-		delete [] a;
-		delete [] results;
-	}
-	delete [] b;
-	delete [] chunk;
-	delete [] chunk_results;
+  if (rank == 0) {
+    auto end = std::chrono::steady_clock::now();
+    auto elapsed = end - start;
+    printf(
+        "task costs %lld μs\n",
+        std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count());
+    delete[] a;
+    delete[] results;
+  }
+  delete[] b;
+  delete[] chunk;
+  delete[] chunk_results;
 
-	MPI_Finalize();
-	return 0;
+  MPI_Finalize();
+  return 0;
 }
--- prime-sieve/main.cc
+++ prime-sieve/main.cc
@@ -1,105 +1,100 @@
-#include "mpi.h"
+#include <chrono>
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
-#include <chrono>
 #include <memory>
 
-int main(int argc, char* argv[])
-{
-	auto timer_start = std::chrono::steady_clock::now();
+#include "mpi.h"
 
-	int rank, size;
+int main(int argc, char *argv[]) {
+  auto timer_start = std::chrono::steady_clock::now();
 
-	int64_t prime;
+  int rank, size;
 
-	if (argc != 2) return 1;
-	int64_t length = atoi(argv[1]);
-	int64_t bound = sqrt(length);
-	int64_t sieve_start = bound + 1;
-	int64_t range = length - bound;
+  int64_t prime;
 
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (argc != 2) return 1;
+  int64_t length = atoi(argv[1]);
+  int64_t bound = sqrt(length);
+  int64_t sieve_start = bound + 1;
+  int64_t range = length - bound;
 
-	if (length % (size - 1) != 0) 
-	{
-		printf("input invalid, program terminated.\n");
-		MPI_Finalize();
-		return -1;
-	}
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	const int source = size - 1;
+  if (length % (size - 1) != 0) {
+    printf("input invalid, program terminated.\n");
+    MPI_Finalize();
+    return -1;
+  }
 
-	int step = ceil(length / (double)(size - 1));
-	int64_t left = sieve_start + step * rank;
-	int64_t right = sieve_start + step * (rank + 1) > length ? length : sieve_start + step * (rank + 1);
-	int64_t offset = left;
-	int64_t count = 0;
+  const int source = size - 1;
 
-	if (rank == source)
-	{
-		bool* array = new bool[bound + 1];
-		memset(array, true, sizeof(bool) * length);
-		for (int64_t i = 2; i <= bound; i++)
-		{
-			if (array[i]) 
-			{
-				prime = i;
-				MPI_Bcast(&prime, 1, MPI_INT64_T, source, MPI_COMM_WORLD);
-				for (int64_t j = pow(i, 2); j <= bound; j += i)
-				{
-					array[j] = false;
-				}
-			}
-		}
-		for (int64_t i = 2; i <= bound; i++) 
-		{
-			if (array[i]) count++;
-		}
-		prime = -1;
-		MPI_Bcast(&prime, 1, MPI_INT64_T, source, MPI_COMM_WORLD);
-		int64_t task_count = 0;
-		for (int i = 0; i < source; i++) 
-		{
-			MPI_Recv(&task_count, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			count += task_count;
-		}
-		printf("for this round we used %d processors, got %lld prime numbers between 0 and %lld\n", size, count, length);
-		delete [] array;
-	}
-	else
-	{
-		bool* task_array = new bool[right - left];
-		memset(task_array, true, sizeof(bool) * (right - left));
-		while (true)
-		{
-			MPI_Bcast(&prime, 1, MPI_INT64_T, source, MPI_COMM_WORLD);
-			if (prime == -1) 
-			{
-				for (int64_t i = left; i < right; i++) 
-				{
-					if (task_array[i - offset]) count++;
-				}
-				MPI_Send(&count, 1, MPI_INT, source, 0, MPI_COMM_WORLD);
-				delete [] task_array;
-				MPI_Finalize();
-				return 0;
-			};
+  int step = ceil(length / (double)(size - 1));
+  int64_t left = sieve_start + step * rank;
+  int64_t right = sieve_start + step * (rank + 1) > length
+                      ? length
+                      : sieve_start + step * (rank + 1);
+  int64_t offset = left;
+  int64_t count = 0;
 
-			int64_t start = ceil(left / (double) prime) * prime;
-			for (int64_t i = start; i < right; i += prime)
-			{
-				task_array[i - offset] = false;
-			}
-		}
-	}
+  if (rank == source) {
+    bool *array = new bool[bound + 1];
+    memset(array, true, sizeof(bool) * length);
+    for (int64_t i = 2; i <= bound; i++) {
+      if (array[i]) {
+        prime = i;
+        MPI_Bcast(&prime, 1, MPI_INT64_T, source, MPI_COMM_WORLD);
+        for (int64_t j = pow(i, 2); j <= bound; j += i) {
+          array[j] = false;
+        }
+      }
+    }
+    for (int64_t i = 2; i <= bound; i++) {
+      if (array[i]) count++;
+    }
+    prime = -1;
+    MPI_Bcast(&prime, 1, MPI_INT64_T, source, MPI_COMM_WORLD);
+    int64_t task_count = 0;
+    for (int i = 0; i < source; i++) {
+      MPI_Recv(&task_count, 1, MPI_INT, i, 0, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      count += task_count;
+    }
+    printf(
+        "for this round we used %d processors, got %lld prime numbers "
+        "between 0 and %lld\n",
+        size, count, length);
+    delete[] array;
+  } else {
+    bool *task_array = new bool[right - left];
+    memset(task_array, true, sizeof(bool) * (right - left));
+    while (true) {
+      MPI_Bcast(&prime, 1, MPI_INT64_T, source, MPI_COMM_WORLD);
+      if (prime == -1) {
+        for (int64_t i = left; i < right; i++) {
+          if (task_array[i - offset]) count++;
+        }
+        MPI_Send(&count, 1, MPI_INT, source, 0, MPI_COMM_WORLD);
+        delete[] task_array;
+        MPI_Finalize();
+        return 0;
+      };
 
-	MPI_Finalize();
+      int64_t start = ceil(left / (double)prime) * prime;
+      for (int64_t i = start; i < right; i += prime) {
+        task_array[i - offset] = false;
+      }
+    }
+  }
 
-	auto timer_end = std::chrono::steady_clock::now();
-	auto elapsed = timer_end - timer_start;
-	printf("and we cost %lld μs\n", std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count());
-	return 0;
+  MPI_Finalize();
+
+  auto timer_end = std::chrono::steady_clock::now();
+  auto elapsed = timer_end - timer_start;
+  printf(
+      "and we cost %lld μs\n",
+      std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count());
+  return 0;
 }
